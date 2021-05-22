3cd140843fa1cd6fb8c779936dfa3d27fce75a81
Dennis von Bargen <dennis@vonbargen.se>
2021-05-22 12:15:10 +0200

    Added linode instance for US server. Awaiting Linode account confirmation

--- main.tf
+++ main.tf
@@ -10,3 +10,24 @@ terraform {
 provider "linode" {
     token = var.token
 }
+
+resource "linode_instance" "vpn-us-se" {
+  label = "vpn-us-se"
+  image = "linode/ubuntu20.04"
+  region = "us-southeast"
+  type = "g6-standard-1"
+  authorized_keys = [var.ssh_key]
+  root_pass = var.root_pass
+
+  connection {
+    type = "ssh"
+    user = "root"
+    password = var.root_pass
+    host = self.ip_address
+  }
+
+  provisioner "file" {
+      source = ""
+      destination = "/tmp/"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@
 variable "token" {}
 variable "root_pass" {}
 variable "ssh_key" {}
-
-# regions
-variable "region-us" {
-    default = "us-southeast"
-}
