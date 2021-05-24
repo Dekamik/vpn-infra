db39214dc61443ced4ec357a7bbecd73508fa332
Dennis von Bargen <dennis@vonbargen.se>
2021-05-24 17:50:32 +0200

    Some minor refactoring

--- main.tf
+++ main.tf
@@ -19,17 +19,21 @@ resource "linode_instance" "vpn-us" {
     authorized_keys = [var.ideapad_key, var.ideapad_key_ed25519, var.legion_key]
 
     provisioner "remote-exec" {
-        inline = ["echo 'export SERVER_NAME=vpn-us' >> ~/.profile", "sudo apt update", "sudo apt install python3 -y"]
+        inline = [
+            "echo 'export SERVER_NAME=vpn-us' >> ~/.profile",
+            "sudo apt update",
+            "sudo apt install python3 -y"
+        ]
 
         connection {
             host = self.ip_address
             type = "ssh"
             user = "root"
-            private_key = "${file(var.pvt_key)}"
+            private_key = file(var.pvt_key)
         }
     }
 
     provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ip_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.ideapad_key}' ovpn-install.yml"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ip_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.ideapad_key}' provisioning/ovpn-install.yml"
     }
 }
